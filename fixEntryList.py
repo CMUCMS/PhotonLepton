c53d49ad2887ad3b2519728f80de156af3942084
Yutaro <yutaro.iiyama@cern.ch>
2014-11-05 21:44:44 +0100

    entry list now gets filled with filter; just need to fix the file name

--- fixEntryList.py
+++ fixEntryList.py
@@ -1,26 +1,57 @@
+import sys
 import os
 import re
 import ROOT
 
 sourceDir = '/store/glskim/'
 
+dataset = sys.argv[1]
+
 for fileName in os.listdir(sourceDir):
-    if not re.match('ZGToLLG(?:|_[0-9]+)[.]root$', fileName): continue
+    if not re.match(dataset + '(?:|_[0-9]+)[.]root$', fileName): continue
 
-    newName = fileName.replace('ZGToLLG', 'ZGToLLG_PtG-5-130')
-    
+    print fileName
+
     skimFile = ROOT.TFile.Open(sourceDir + fileName, 'update')
-    if skimFile.Get('entrylist'):
-        skimFile.Close()
-        continue
+    hardPhotons = skimFile.Get('hardPhotons')
+    softPhotons = skimFile.Get('softPhotons')
 
-    tree = skimFile.Get('eventVars')
-    tree.Draw('>>entrylist', 'lhePhotonPt < 130.', 'entrylist')
+    cut = ''
+
+    if dataset == 'ZGToLLG':
+        oldName = fileName
+        fileName = fileName.replace('ZGToLLG', 'ZGToLLG_PtG-5-130')
+
+        cut = 'lhePhotonPt < 130.'
+
+    elif dataset == 'TTJetsFullLept' or dataset == 'TTJetsSemiLept' or dataset == 'WW':
+        cut = 'radPt < 20. || isFSR'
+
+    if cut:
+        tree = skimFile.Get('eventVars')
+
+        hardPhotons.SetFileName(sourceDir + fileName)
+        hardPhotons.SetName('oldHardPhotons')
+        tree.SetEntryList(hardPhotons)
+        tree.GetEntryNumber(0) # needed to initialize the list
+        tree.Draw('>>hardPhotons', cut, 'entrylist')
+        hardPhotons = ROOT.gDirectory.Get('hardPhotons')
     
-    elist = ROOT.gDirectory.Get('entrylist')
-    elist.SetFileName('rooth://ncmu40/' + sourceDir + newName)
+        softPhotons.SetFileName(sourceDir + fileName)
+        softPhotons.SetName('oldSoftPhotons')
+        tree.SetEntryList(softPhotons)
+        tree.GetEntryNumber(0) # needed to initialize the list
+        tree.Draw('>>softPhotons', cut, 'entrylist')        
+        softPhotons = ROOT.gDirectory.Get('softPhotons')
 
-    elist.Write()
+        tree.SetEntryList(0)
+
+    hardPhotons.SetFileName('rooth://ncmu40/' + sourceDir + fileName)
+    hardPhotons.Write()
+    softPhotons.SetFileName('rooth://ncmu40/' + sourceDir + fileName)
+    softPhotons.Write()
+
     skimFile.Close()
-
-    os.rename(sourceDir + fileName, sourceDir + newName)
+    
+    if dataset == 'ZGToLLG':
+        os.rename(sourceDir + oldName, sourceDir + fileName)
